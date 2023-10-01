69673cc314a9fdab650fd3ca3f81121bd19a6afe
Jorge Castro <nikey_es@yahoo.es>
2023-10-02 00:15:43 +0200

    Try to simplify json

--- download_prices.py
+++ download_prices.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from datetime import datetime, timedelta
 import requests
 import json
@@ -11,74 +12,163 @@ from rich.progress import (
     TimeRemainingColumn,
 )
 
+class HotelRate:
+    date:str
+    name:str
+    rate:float
+    rateOld:float
+    discount:float
 
-date_ini = datetime(2023, 10, 1)
-date_end = datetime(2023, 10, 4)
+    def __init__(self, date:datetime, name:str, rate:float, rateOld:float, discount:float) -> None:
+        self.date = date.strftime("%Y-%m-%d")
+        self.name = name
+        self.rate = rate
+        self.rateOld = rateOld
+        self.discount = discount
 
-# Definir el paso (en este caso, un día)
-step = timedelta(days=1)
+@dataclass
+class PortaventuraRates:
+    download_date_start:str
+    download_date_end:str
+    query:dict
+    hotels_rate=[]
 
-# Iterar a través del intervalo de fechas
-current_date = date_ini
-num_days = (date_end-date_ini).days + 1
-result = {}
-
-progress_columns = (
-            SpinnerColumn(),
-            "[progress.description]{task.description}",
-            BarColumn(),
-            TaskProgressColumn(),
-            "Elapsed:",
-            TimeElapsedColumn(),
-            "Remaining:",
-            TimeRemainingColumn(),
-        )
-with Progress(*progress_columns, transient=False) as progress:
-    analysing_task = progress.add_task(
-                f"[cyan]Downloading {num_days} days", start=True, total=num_days
-            )
-
-    while current_date <= date_end:
-
-        reqUrl = "https://book.portaventuraworld.com/funnel/hotels/chain"
-
-        headersList = {
-        "Content-Type": "application/json" 
-        }
-
-        endDate = current_date+timedelta(days=1)
-        payload = json.dumps({
-        "languageCode": "es",
-        "startDate": current_date.strftime("%Y-%m-%d"),
-        "endDate": endDate.strftime("%Y-%m-%d"),
-        "children": 2,
-        "adults": 3,
-        "childrenAges": [6,10],
-        "rooms": 1,
-        "maxChildren": 2,
-        "maxAdults": 3,
-        "coupon": "",
-        "couponType": "Discount",
-        "roomsArray": [
-            {
-            "adults": 3,
-            "children": 2,
-            "childrenAges": [6,10]
-            }
-        ]
-        })
-
-        response = requests.request("POST", reqUrl, data=payload,  headers=headersList)
-        
-        result[current_date.strftime("%Y-%m-%d")] =  json.loads(response.text)
-
-        current_date += step
-
-        progress.update(analysing_task, advance=1)
-
-    progress.update(analysing_task, completed=num_days)
+    def __init__(self, date_start:datetime, date_end:datetime) -> None:
+        self.download_date_start = date_start.strftime("%Y-%m-%d")
+        self.download_date_end = date_end.strftime("%Y-%m-%d")
+        self.query = {
+                "languageCode": "es",
+                "startDate": None,
+                "endDate": None,
+                "children": 2,
+                "adults": 3,
+                "childrenAges": [6,10],
+                "rooms": 1,
+                "maxChildren": 2,
+                "maxAdults": 3,
+                "coupon": "",
+                "couponType": "Discount",
+                "roomsArray": [
+                    {
+                    "adults": 3,
+                    "children": 2,
+                    "childrenAges": [6,10]
+                    }
+                ]
+                }
     
- 
-with open(f'downloaded_data/{datetime.today().strftime("%Y%m%d")}.json', 'w') as archivo:
-    json.dump(result, archivo, ensure_ascii=False, indent=4)
+    def get_query(self, start_date:datetime, end_date:datetime):
+        self.query["startDate"] = start_date.strftime("%Y-%m-%d")
+        self.query["endDate"] = end_date.strftime("%Y-%m-%d")
+        return self.query
+    
+    def add_hotel(self, hotel: HotelRate):
+        self.hotels_rate.append(hotel)
+
+    def to_json(self):
+        data = self.__dict__.copy()
+    
+        hotel_list = [hotel.__dict__ for hotel in self.hotels_rate]
+
+        data["hotels_rate"] = hotel_list
+        
+        return json.dumps(data, ensure_ascii=False, indent=4)
+    
+
+
+
+class DownloadPrices:
+    date_ini:datetime
+    date_end:datetime
+    step:timedelta = timedelta(days=1)
+
+    def __init__(self, date_ini:datetime, date_end:datetime) -> None:
+        if date_ini is None:
+            raise ValueError("date_ini is mandatory")
+    
+        if not isinstance(date_ini, datetime):
+            raise TypeError("date_ini must be of type datetime")
+        
+        if date_end is None:
+            raise ValueError("date_end is mandatory")
+    
+        if not isinstance(date_end, datetime):
+            raise TypeError("date_end must be of type datetime")
+        
+        self.date_ini = date_ini
+        self.date_end = date_end
+
+    def download(self):
+        current_date = self.date_ini
+        num_days = (self.date_end - self.date_ini).days + 1
+
+        progress_columns = (
+                    SpinnerColumn(),
+                    "[progress.description]{task.description}",
+                    BarColumn(),
+                    TaskProgressColumn(),
+                    "Elapsed:",
+                    TimeElapsedColumn(),
+                    "Remaining:",
+                    TimeRemainingColumn(),
+                )
+        with Progress(*progress_columns, transient=False) as progress:
+            analysing_task = progress.add_task(
+                        f"[cyan]Downloading {num_days} days", start=True, total=num_days
+                    )
+            
+            portaventura_rates = PortaventuraRates(date_start= self.date_ini ,date_end= self.date_end)
+
+            while current_date <= self.date_end:
+
+                reqUrl = "https://book.portaventuraworld.com/funnel/hotels/chain"
+
+                headersList = {
+                "Content-Type": "application/json" 
+                }
+
+                endDate = current_date+timedelta(days=1)
+                payload = json.dumps(
+                            portaventura_rates.get_query(start_date=current_date, 
+                                                        end_date=endDate)
+                                    )
+
+                response = requests.request("POST", reqUrl, data=payload,  headers=headersList)
+                
+                rates_json = json.loads(response.text)
+                
+                name=None
+                rate=None
+                rateOld=None
+                discount=None
+                for hotel in rates_json["hotels"]:
+                    if "hotelName" in hotel:
+                        name = hotel["hotelName"]
+                    if "ratePlan" in hotel and hotel["ratePlan"] is not None:
+                        rate_plan = hotel["ratePlan"]
+                        if "rate" in rate_plan:
+                            rate = rate_plan["rate"]
+                        if "rateOld" in rate_plan:
+                            rateOld = rate_plan["rateOld"]
+                        if "discount" in rate_plan:
+                            discount = rate_plan["discount"]
+
+                    hotel_rate = HotelRate(date=current_date,
+                                           name=name,
+                                           rate=rate,
+                                           rateOld=rateOld,
+                                           discount=discount)
+                    
+                    
+                    portaventura_rates.add_hotel(hotel=hotel_rate)
+
+                current_date += self.step
+
+                progress.update(analysing_task, advance=1)
+
+            progress.update(analysing_task, completed=num_days)
+            
+        
+        with open(f'downloaded_data/{datetime.today().strftime("%Y%m%d")}.json', 'w') as archivo:
+            archivo.write(portaventura_rates.to_json())
 
--- find_offers.py
+++ find_offers.py
@@ -2,57 +2,51 @@ from dataclasses import dataclass
 import datetime
 import json
 
-@dataclass
-class Rate:
-    date: datetime
-    hotel_name: str
-    rate: float
-
-
 class FindOffers:
     data :json = None
-    rates = []
+    hotels_rate = []
 
     def __init__(self, data_file:str) -> None:
         file_path = data_file
 
         with open(file_path, 'r') as json_file:
             self.data = json.load(json_file)
+            self.hotels_rate = self.data["hotels_rate"]
 
-        for day, value in self.data.items():
-            if value.get("hotels"):
-                for hotel in value["hotels"]:
-                    price = hotel["ratePlan"]["rate"] if hotel["ratePlan"] else 999999
-                    rate = Rate(date=day, hotel_name= hotel["hotelName"], rate=price)
-                    self.rates.append(rate)
+        # for day, value in self.data.items():
+        #     if value.get("hotels"):
+        #         for hotel in value["hotels"]:
+        #             price = hotel["ratePlan"]["rate"] if hotel["ratePlan"] else 999999
+        #             rate = Rate(date=day, hotel_name= hotel["hotelName"], rate=price)
+        #             self.rates.append(rate)
 
 
     def print_unique_hotel_names(self):
         unic_names = set()
 
-        for rate in self.rates:
-            unic_names.add(rate.hotel_name)
+        for hotel_rate in self.hotels_rate:
+            unic_names.add(hotel_rate["name"])
 
-        print("Unique hotel names:")
+        print("------------Unique hotel names:---------------")
         for name in unic_names:
             print(name)
 
     def print_minor_rates_only_port_aventura(self):
         only_this_hotels = ["Hotel El Paso", "Hotel Colorado Creek","Hotel Gold River", "Hotel Mansión de Lucy", "Hotel PortAventura", "Hotel Caribe", "Hotel Roulette"]
 
-        filtered_hotels = [rate for rate in self.rates if rate.hotel_name in only_this_hotels]
+        filtered_hotels = [hotel_rate for hotel_rate in self.hotels_rate if hotel_rate.name in only_this_hotels]
 
         self.minor_rate(filtered_hotels)
 
     def print_minor_rates_only_this_hotel(self, hotel:str):
 
-        filtered_hotels = [rate for rate in self.rates if rate.hotel_name == hotel]
+        filtered_hotels = [hotel_rate for hotel_rate in self.hotels_rate if hotel_rate.name == hotel]
 
         self.minor_rate(filtered_hotels)
 
     
     def print_minor_rates_all_hotels(self):
-            self.minor_rate(self.rates)
+            self.minor_rate(self.hotels_rate)
 
 
     def minor_rate(self, hotels):
@@ -65,33 +59,17 @@ class FindOffers:
 
         # Imprimir los 5 objetos con las tasas más bajas
         print("-------------Lowest rates:---------------")
-        for rate in five_minor_rates:
-            print(f"Date: {rate.date}, Hotel Name: {rate.hotel_name}, Rate: {rate.rate}")
+        for hotel_rate in five_minor_rates:
+            print(f"Date: {hotel_rate.date}, Hotel Name: {hotel_rate.name}, Rate: {hotel_rate.rate}")
 
 
     def print_last_date_with_rate(self):
-        sorted_dates = sorted(self.data.keys(), reverse=True)
+        result = max(
+            (hotel for hotel in self.hotels_rate if hotel["rate"] is not None),
+            key=lambda x: x.date
+            )
 
-        # Find the last date with a non-null rate
-        last_date = None
-        for date in sorted_dates:
-            if date in self.data and "hotels" in self.data[date]:
-                ratePlan = self.data[date]["hotels"][0]["ratePlan"]
-                if ratePlan is not None:
-                    last_date = date
-                    break
-
-        if last_date:
-            print("The last date with a non-null rate is:", last_date)
+        if result:
+            print("The last date with a non-null rate is:",  result.date, result.name, result.rate)
         else:
             print("No non-null rates were found on any date.")
-
-find_offers = FindOffers('./downloaded_data/20230930_datos_3_adult_2_child.json')
-find_offers.print_unique_hotel_names()
-print("----------------------------------")
-find_offers.print_last_date_with_rate()
-find_offers.print_minor_rates_all_hotels()
-find_offers.print_minor_rates_only_port_aventura()
-find_offers.print_minor_rates_only_this_hotel("Hotel Caribe")
-find_offers.print_minor_rates_only_this_hotel("Hotel Mansión de Lucy")
-find_offers.print_minor_rates_only_this_hotel("Hotel Colorado Creek")
