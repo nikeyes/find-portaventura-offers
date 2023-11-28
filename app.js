da67aa237d498869a1dcf82068da3b256181e8ba
Jorge Castro <nikey_es@yahoo.es>
2023-11-28 07:40:26 +0100

    New page

---
+++ app.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetchData();
+});
+
+async function fetchData() {
+    try {
+        const response = await fetch('https://raw.githubusercontent.com/nikeyes/find-portaventura-offers/main/downloaded_data/hotels_20231117_a3_c2_6_9.json');
+        const data = await response.json();
+        displayData(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+function displayData(data) {
+    const appContainer = document.getElementById('app');
+
+    const container = document.createElement('div');
+    container.classList.add('container');
+
+    const table = document.createElement('table');
+    table.classList.add('table');
+
+    // Create table headers
+    const headers = ['Fecha', 'Nombre del Hotel', 'Tarifa', 'Descuento'];
+    const headerRow = document.createElement('tr');
+    headers.forEach(headerText => {
+        const th = document.createElement('th');
+        th.textContent = headerText;
+        headerRow.appendChild(th);
+    });
+    table.appendChild(headerRow);
+
+    // Populate table with data
+    data.hotels_rate.forEach(item => {
+        const row = document.createElement('tr');
+        const dateCell = document.createElement('td');
+        dateCell.textContent = item.date;
+        row.appendChild(dateCell);
+
+        const nameCell = document.createElement('td');
+        nameCell.textContent = item.name;
+        row.appendChild(nameCell);
+
+        const rateCell = document.createElement('td');
+        rateCell.textContent = item.rate;
+        row.appendChild(rateCell);
+
+        const discountCell = document.createElement('td');
+        discountCell.textContent = item.discount;
+        row.appendChild(discountCell);
+
+        table.appendChild(row);
+    });
+
+    container.appendChild(table);
+    appContainer.appendChild(container);
+}
