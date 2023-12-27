66e4fc9af798952780888b48fa1c462638f96185
Jorge Castro <jorge.castro@adevinta.com>
2023-12-27 13:15:41 +0100

    Refactor to use assert_that from assertpy

--- tests/test_portaventura_occupancy.py
+++ tests/test_portaventura_occupancy.py
@@ -1,11 +1,12 @@
-import unittest
+from unittest import TestCase
+from assertpy import assert_that
 from src.commands.download_tickets_prices import TicketPrice
 from src.commands.occupancy import Occupancy
 
-class TestOccupancy(unittest.TestCase):
 
+class TestOccupancy(TestCase):
     def setUp(self):
-        ticket_prices = [ 
+        ticket_prices = [
             TicketPrice(date="2022-01-01", price=60),
             TicketPrice(date="2022-01-02", price=60),
             TicketPrice(date="2022-01-03", price=40),
@@ -13,26 +14,26 @@ class TestOccupancy(unittest.TestCase):
             TicketPrice(date="2022-01-05", price=40),
             TicketPrice(date="2022-01-06", price=50),
             TicketPrice(date="2022-01-07", price=52),
-            TicketPrice(date="2022-01-08", price=48)
-            ]
+            TicketPrice(date="2022-01-08", price=48),
+        ]
         self.occupancy = Occupancy(ticket_prices=ticket_prices)
 
     def test_get_occupancy_high_day(self):
         result = self.occupancy.get_occupancy("2022-01-01")
-        self.assertEqual(result, "high")
-    
+        assert_that(result).is_equal_to("high")
+
     def test_get_occupancy_second_high_day(self):
         result = self.occupancy.get_occupancy("2022-01-07")
-        self.assertEqual(result, "high")
+        assert_that(result).is_equal_to("high")
 
     def test_get_occupancy_low_day(self):
         result = self.occupancy.get_occupancy("2022-01-03")
-        self.assertEqual(result, "low")
-    
+        assert_that(result).is_equal_to("low")
+
     def test_get_occupancy_second_low_day(self):
         result = self.occupancy.get_occupancy("2022-01-04")
-        self.assertEqual(result, "low")
+        assert_that(result).is_equal_to("low")
 
     def test_get_occupancy_nonexistent_day(self):
         result = self.occupancy.get_occupancy("2022-01-09")
-        self.assertIsNone(result)
+        assert_that(result).is_none()
--- tests/test_tickets_prices.py
+++ tests/test_tickets_prices.py
@@ -1,78 +1,72 @@
-import unittest
+from unittest import TestCase
+from assertpy import assert_that
 from src.commands.download_tickets_prices import TicketPrice
 from src.commands.occupancy import Occupancy
 
-class TestDownloadTicketPrices(unittest.TestCase):
 
+class TestDownloadTicketPrices(TestCase):
     def test_get_lowest_price(self):
-        
-        ticket_prices = [ 
+        ticket_prices = [
             TicketPrice(date="2022-01-01", price=50),
             TicketPrice(date="2022-01-02", price=60),
             TicketPrice(date="2022-01-03", price=40),
             TicketPrice(date="2022-01-04", price=60),
             TicketPrice(date="2022-01-05", price=40),
-            ]
-        
+        ]
+
         occupancy = Occupancy(ticket_prices=ticket_prices)
-        
+
         lowest_price = occupancy.get_lowest_price()
 
-        self.assertEqual(lowest_price, 40)
-    
-    def test_get_higher_price(self):
+        assert_that(lowest_price).is_equal_to(40)
 
-        ticket_prices = [ 
+    def test_get_higher_price(self):
+        ticket_prices = [
             TicketPrice(date="2022-01-01", price=52),
             TicketPrice(date="2022-01-02", price=60),
             TicketPrice(date="2022-01-03", price=48),
             TicketPrice(date="2022-01-04", price=60),
             TicketPrice(date="2022-01-05", price=40),
-            ]
-        
+        ]
+
         occupancy = Occupancy(ticket_prices=ticket_prices)
 
         lowest_price = occupancy.get_higher_price()
 
-        self.assertEqual(lowest_price, 60)
+        assert_that(lowest_price).is_equal_to(60)
 
     def test_get_second_lowest_price(self):
-        
-        ticket_prices = [ 
+        ticket_prices = [
             TicketPrice(date="2022-01-01", price=50),
             TicketPrice(date="2022-01-02", price=60),
             TicketPrice(date="2022-01-03", price=40),
             TicketPrice(date="2022-01-04", price=60),
             TicketPrice(date="2022-01-05", price=40),
-            ]
+        ]
 
         occupancy = Occupancy(ticket_prices=ticket_prices)
-        
+
         second_lowest_price = occupancy.get_second_lowest_price()
 
-        self.assertEqual(second_lowest_price, 50)
-    
+        assert_that(second_lowest_price).is_equal_to(50)
+
     def test_get_second_higher_price(self):
-        
-        ticket_prices = [ 
+        ticket_prices = [
             TicketPrice(date="2022-01-01", price=50),
             TicketPrice(date="2022-01-02", price=60),
             TicketPrice(date="2022-01-03", price=40),
             TicketPrice(date="2022-01-04", price=60),
             TicketPrice(date="2022-01-05", price=52),
-            ]
+        ]
 
         occupancy = Occupancy(ticket_prices=ticket_prices)
 
         second_lowest_price = occupancy.get_second_higher_price()
 
-        self.assertEqual(second_lowest_price, 52)
+        assert_that(second_lowest_price).is_equal_to(52)
 
-
-    
     def test_get_dates_with_occupancy(self):
-        
-        ticket_prices = [ 
+        ticket_prices = [
             TicketPrice(date="2022-01-01", price=60),
             TicketPrice(date="2022-01-02", price=60),
             TicketPrice(date="2022-01-03", price=40),
@@ -80,18 +74,23 @@ class TestDownloadTicketPrices(unittest.TestCase):
             TicketPrice(date="2022-01-05", price=40),
             TicketPrice(date="2022-01-06", price=50),
             TicketPrice(date="2022-01-07", price=52),
-            TicketPrice(date="2022-01-08", price=48)
-            ]
-        
+            TicketPrice(date="2022-01-08", price=48),
+        ]
+
         occupancy = Occupancy(ticket_prices=ticket_prices)
         result = occupancy.get_dates_with_occupancy_low_high()
 
-        expected_result = [{'day': '2022-01-01', 'price': 60, "occupation":"high"},
-                            {'day': '2022-01-02', 'price': 60, "occupation":"high"},
-                            {'day': '2022-01-03', 'price': 40, "occupation":"low"},
-                            {'day': '2022-01-04', 'price': 48, "occupation":"low"},
-                            {'day': '2022-01-05', 'price': 40, "occupation":"low"},
-                            {'day': '2022-01-07', 'price': 52, "occupation":"high"},
-                            {'day': '2022-01-08', 'price': 48, "occupation":"low"}]
+        expected_result = [
+            {'day': '2022-01-01', 'price': 60, "occupation": "high"},
+            {'day': '2022-01-02', 'price': 60, "occupation": "high"},
+            {'day': '2022-01-03', 'price': 40, "occupation": "low"},
+            {'day': '2022-01-04', 'price': 48, "occupation": "low"},
+            {'day': '2022-01-05', 'price': 40, "occupation": "low"},
+            {'day': '2022-01-07', 'price': 52, "occupation": "high"},
+            {'day': '2022-01-08', 'price': 48, "occupation": "low"},
+        ]
 
         self.assertEqual(result, expected_result)
+        # The error message of self.assertEquals is much better
+        # than that of assertpy for comparing lists or dictionaries,
+        # assert_that(result).is_equal_to(expected_result)
